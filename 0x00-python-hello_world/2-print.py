7519ba25cea28a828f536c58cfbb9d88979aefcc
Orah <orahchiedoziem@gmail.com>
2022-11-29 15:40:20 +0000

    c

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-number = 98
-if number == 98:
-    print("\"Programming is like building a multilingual puzzle\n")
+print("\"Programming is like building a multilingual puzzle\n")
